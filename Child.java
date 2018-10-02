a71c5396254305b4755bfa2e400b18e28310a97a
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-10-01 17:41:07 -0700

    Create Child.java

---
+++ Child.java
@@ -0,0 +1,13 @@
+//Programmer: Jennifer King
+
+public class Child implements Player 
+{
+	@Override
+	public String play()
+	{
+		String str;
+		str = "I am a child and I am playing a game";
+		return str;
+	}
+
+}
