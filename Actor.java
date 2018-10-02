c70bc970a0ca27f3cb38e347d4161b93c5016f3b
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-10-01 17:41:33 -0700

    Create Actor.java

---
+++ Actor.java
@@ -0,0 +1,12 @@
+//Programmer: Jennifer King
+
+public class Actor implements Player
+{
+	@Override
+	public String play()
+	{
+		String str;
+		str = "I am an actor and I am playing a part";
+		return str;
+	}
+}
