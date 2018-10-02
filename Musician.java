13f9bfdfb370be30e2f9a536b1c966160d675dbd
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-10-01 17:41:20 -0700

    Create Musician.java

---
+++ Musician.java
@@ -0,0 +1,12 @@
+//Programmer: Jennifer King
+
+public class Musician implements Player
+{
+	@Override
+	public String play()
+	{
+		String str;
+		str = "I am a musician and I am playing a song";
+		return str;
+	}
+}
